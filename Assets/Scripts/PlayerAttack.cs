7b1e3c75ef5a73d223c40ab832109bdc02ba5f70
SYLVIO ANTONIO DE O. C. FILHO <16180510@pucrs.br>
2019-11-01 10:43:48 -0300

    first commit

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public Transform attackPos;
+    public float attackRange;
+    public LayerMask enemyLayerMask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")) {
+            Collider2D[] enemiesHit = Physics2D.OverlapCircleAll(attackPos.position, attackRange, enemyLayerMask);
+            foreach(Collider2D e in enemiesHit) {
+                Debug.Log("enemy hit!!");
+                e.GetComponent<EnemyState>().TakeDamage(10);
+            }
+        }
+    }
+
+    void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float walkSpeed = 5f;
+    bool isWalking;
+    Rigidbody2D rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        isWalking = false;
+    }
+
+    void Update()
+    {
+        float inputH = Input.GetAxis("Horizontal");
+        float inputV = Input.GetAxis("Vertical");
+        
+        float deltaH = inputH * walkSpeed;
+        float deltaV = inputV * walkSpeed;
+        
+        if(inputH != 0 || inputV != 0) {
+            isWalking = true;
+        }
+        
+        Vector2 vel = new Vector2(deltaH, deltaV);
+        rigidBody.velocity = vel;
+    }
+}
