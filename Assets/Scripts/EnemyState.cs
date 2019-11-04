8872fb40da2c0d045fc32e9d2f45c6c794d4d731
SYLVIO ANTONIO DE O. C. FILHO <16180510@pucrs.br>
2019-11-04 14:09:53 -0200

    enemy pursuit

--- Assets/Scripts/EnemyState.cs
+++ Assets/Scripts/EnemyState.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class EnemyState : MonoBehaviour
 {
     public int health;
+    float hurt;
     // Start is called before the first frame update
     void Start()
     {
         health = 100;
+        hurt = 0f;
     }
 
     // Update is called once per frame
@@ -17,10 +19,18 @@ public class EnemyState : MonoBehaviour
         if(health <= 0) {
             Destroy(gameObject);
         }
+        if(hurt > 0) {
+            hurt -= Time.deltaTime;
+        } else {
+            GetComponent<SpriteRenderer>().color = Color.white;
+        }
+
     }
 
     public void TakeDamage(int damage) {
         health -= damage;
         Debug.Log("Damage taken");
+        GetComponent<SpriteRenderer>().color = Color.red;
+        hurt = 0.5f;
     }
 }
---
+++ Assets/Scripts/Pursuit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pursuit : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+    public float alertDistance;
+    public float targetDistance;
+    public Rigidbody2D rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector2.Distance(transform.position, target.position);
+        if(distance > targetDistance && distance < alertDistance) {
+            Vector3 vec = target.position - transform.position;
+            rigidBody.velocity = vec.normalized * speed;
+        }
+    }
+}
