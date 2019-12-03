b0e70e2bd7bb52f516158dd6fd06190b06466ab0
SYLVIO ANTONIO DE O. C. FILHO <16180510@pucrs.br>
2019-12-03 09:23:41 -0300

    vanishing effect for potion

---
+++ Assets/Scripts/Vanishing.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vanishing : MonoBehaviour
+{
+    public float timeToVanish;
+    public float timeLeft;
+    public bool isVanishing;
+    public Animator animator;
+    
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLeft = timeToVanish;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isVanishing) {
+            if(timeLeft <= timeToVanish * 0.4) {
+                isVanishing = true;
+                animator.Play("PotionVanish");
+            }
+        }
+        
+        if(timeLeft <= 0) {
+            Destroy(gameObject);
+        } else {
+            timeLeft -= Time.deltaTime;
+        }
+    }
+}
